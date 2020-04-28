062ec862c98603d73ba8082ebc1dc5a83b38851f
joshzindler <joshzindler1498@gmail.com>
2020-04-28 02:21:44 -0700

    restructure

--- beatdappchallenge-master/src/index.js
+++ beatdappchallenge-master/src/index.js
@@ -4,7 +4,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./src/App";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
