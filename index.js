e7607ebefe475be297c04d5d3aae6792847af984
joshzindler <joshzindler1498@gmail.com>
2020-04-28 02:31:44 -0700

    restructure

---
+++ index.js
@@ -0,0 +1,15 @@
+// const React = require("react");
+// const ReactDOM = require("react-dom");
+// const App = require("./App");
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./src/App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
