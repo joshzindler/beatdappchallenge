b5462b32929095ef59d2d4ee3c74fc02d2afee64
joshzindler <joshzindler1498@gmail.com>
2020-04-28 01:35:42 -0700

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "./components/Button";
 
-function App() {
+var total = 0;
+
+class ExpenseForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: total, text: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleClick(event) {
+    this.setState({ value: event.target.value });
+    total = Number(total) + Number(this.state.value);
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>${total}</h2>
+        <form>
+          <label>
+            <input
+              type="text"
+              //value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+        </form>
+        <br />
+        <Button
+          onClick={this.handleClick}
+          type="button"
+          buttonStyle="btn--primary--outline"
+          buttonSize="btn--large"
+        >
+          Record Purchase
+        </Button>
+        <br />
+        <br />
+        Expense Comments:
+        <br />
+        <textarea type="text" value={this.text} />
+        <br />
+        <br />
+      </div>
+    );
+  }
+}
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>beatdapp Expense Tracker</h1>
+      <span> </span>
+      <br />
+      Amount:
+      <ExpenseForm>
+        <label>
+          <input type="text" name="amount" />
+        </label>
+      </ExpenseForm>
     </div>
   );
 }
-
-export default App;
